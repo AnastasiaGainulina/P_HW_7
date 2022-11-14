3a8bab34474dddbecb262438b1fca49790abfaee
AnastasiaGainulina <109802756+AnastasiaGainulina@users.noreply.github.com>
2022-11-14 16:30:01 +0300

    Create filling_csv_file.py

---
+++ filling_csv_file.py
@@ -0,0 +1,7 @@
+from UI import get_dict as get
+
+dict = get()
+def write_csv ():
+    file = 'Phone_dictionary.csv'
+    with open (file, 'a', encoding = 'utf-8') as data:
+        data.write(f'{dict[0]};{dict[1]};{dict[2]};{dict[3]};{dict[4]}')
